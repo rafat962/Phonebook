1e33c396c7f19695417c3f5bbd898c2a9a23ebbf
rafat962 <rafatkamel96@gmail.com>
2026-04-05 12:55:34 +0200

    step4

--- models/Person.js
+++ models/Person.js
@@ -1,8 +1,27 @@
 import mongoose from "mongoose";
 
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err.message));
+
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minlength: [3, "Name must be at least 3 characters long"],
+        required: [true, "Name is required"],
+    },
+
+    number: {
+        type: String,
+        required: [true, "Number is required"],
+        minlength: [8, "Number must be at least 8 characters long"],
+        validate: {
+            validator: (v) => /^\d{2,3}-\d+$/.test(v),
+            message: (props) =>
+                `${props.value} is not a valid phone number! Format must be XX-XXXXXXXX or XXX-XXXXXXXX`,
+        },
+    },
 });
 
 personSchema.set("toJSON", {
